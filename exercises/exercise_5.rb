583895e41cd1f415df4b0f5918c0607d7b43a517
andrijana85 <topalovic.andrijana@yahoo.com>
2023-10-20 13:14:04 -0400

    use sum, average and count to do calculation

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+## Output the total revenue for the entire company (all stores),
+##  using Active Record's `.sum` calculation method.
+
+puts "Total revenue: #{Store.sum(:annual_revenue)}"
+
+## Output the average annual revenue for all stores.
+
+puts "Average annual revenue: #{Store.average(:annual_revenue)}"
+
+## Output the number of stores that are generating $1M or more in annual sales.
+##  **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
+
+puts "Number of stores generating $1M or more in annual sales: #{Store.where("annual_revenue >= ?", 1000000).count}"
